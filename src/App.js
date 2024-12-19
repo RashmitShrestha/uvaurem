5b4b6fd532f7f0437e9dc40d310a4eaa6951e293
RashmitShrestha <rashmit.stha@gmail.com>
2024-12-19 13:51:50 -0800

    added spotify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Timer from './components/timer/Timer.js';
 import Navb from './components/navbar/Navb.js';
 import TaskList from './components/tasklist/Tasklist.js';
-import{Row, Col, Container} from 'react-bootstrap';
+import Spotify from './components/islands/spotify.js';
+import { Row, Col, Container } from 'react-bootstrap';
 import { useState, useRef } from 'react';
 
 //react bootstrap form input
@@ -26,9 +27,9 @@ function App() {
   const brSec = useRef();
 
   const submFunc = (e) => {
-    setWrkT(Number((wrMin.current.value * 60) + wrSec.current.value) );
-    setBrkT(Number((brMin.current.value * 60) + brSec.current.value ));
-    e.preventDefault(); 
+    setWrkT(Number((wrMin.current.value * 60) + wrSec.current.value));
+    setBrkT(Number((brMin.current.value * 60) + brSec.current.value));
+    e.preventDefault();
   }
 
   return (
@@ -39,32 +40,42 @@ function App() {
           wrSec: wrSec,
           brMin: brMin,
           brSec: brSec,
-          subF : submFunc
-          
-        }}/>
+          subF: submFunc
+
+        }} />
 
 
 
         <Container >
 
-          <Row height="100vh">
+          <Row height="100vh" style={{ marginBottom: "10px" }}>
             <Col md={8} className="mx-auto ">
-            <div className="islands">
-            <Timer wrkT={wrkTi} brkT={brkTi} wOB={true} />
-
-            </div>
-
+              <div className="islands">
+                <Timer wrkT={wrkTi} brkT={brkTi} wOB={true} />
+              </div>
             </Col>
 
             <Col md={4} className="mx-auto  font-white">
-            <div className="islands">
-              <h3>Task List</h3>
-              <TaskList />
-
-            </div>
+              <div className="islands">
+                <h3>Task List</h3>
+                <TaskList />
+              </div>
             </Col>
-
           </Row>
+
+          <Row>
+            <Col md={7}>
+              <div className="islands">
+                <Spotify />
+              </div>
+            </Col>
+            <Col md={5}>
+              <div className="islands">
+                <h1> Motivation Quotes</h1>
+              </div>
+            </Col>
+          </Row>
+
         </Container>
         {/* <iframe style={{backgroundColor: "blue", padding: "5px", maxWidth:"1000"}} src="https://open.spotify.com/embed/playlist/4gKYOkQ58e3bnMttLS2p3X?utm_source=generator" width="auto" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
       </section>
---
+++ src/components/islands/spotify.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spotify = () => {
+    return (
+        <div>
+            <h1>Spotify</h1>
+            <iframe src="https://open.spotify.com/embed/playlist/4gKYOkQ58e3bnMttLS2p3X?utm_source=generator" width="auto" height="400px"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+    );
+};
+
+export default Spotify;
